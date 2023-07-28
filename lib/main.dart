146b09d4a0a25c811ab21ea3466a3faf6a77e0df
David Mwalimu Nzambuli <nzambulidavid@gmail.com>
2023-07-28 12:52:51 +0300

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:mile_two/screens/movie_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+   
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
   @override
+  
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Movie Application',
@@ -22,7 +30,12 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
+      
       home: MovieScreen(),
+      
     );
+    
   }
+  
 }
+
--- lib/screens/movie_screen.dart
+++ lib/screens/movie_screen.dart
@@ -1,71 +1,20 @@
 import 'package:flutter/material.dart';
 import '../models/movie.dart';
 
-class MovieScreen extends StatelessWidget {
-  final List<Movie> favoriteMovies = [
-    Movie(
-        director: "Christopher Nolan",
-        releaseYear: 2023,
-        title: "Oppenheimer",
-        posterUrl:
-            "https://movies.universalpictures.com/media/opr-tsr1sheet3-look2-rgb-3-1-1-64545c0d15f1e-1.jpg"),
-    Movie(
-        director: "Zack Snyder",
-        releaseYear: 2013,
-        title: "Man of Steel",
-        posterUrl:
-            "http://2.bp.blogspot.com/-V4wozefHUw4/UYQSw_Nhc4I/AAAAAAAAAL8/eY5-aDts1eI/s1600/man-of-steel-+new+flying+poster.jpg"),
-    Movie(
-        director: 'Quentin Tarantino',
-        title: 'Kill BIll Vol.1',
-        releaseYear: 2003,
-        posterUrl:
-            "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT2n6mrijVjYMAFEQ1DUaKCp0P02YwVyzAO9Hf1ypNEPvsJk835"),
-    Movie(
-        director: "David Fincher",
-        releaseYear: 1999,
-        title: "fight club",
-        posterUrl:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSw3__ptLTHyk_K5QoGfL7OCzXU05LncsZM1A&usqp=CAU"),
-    Movie(
-        director: "Josh Boone",
-        releaseYear: 2014,
-        title: "The Fault in Our Stars",
-        posterUrl:
-            "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR6N8kOvrQrjIEBjIFjcP4qXTSSETEoR1J2CeFl5gtlcufQ9PKP"),
-    Movie(
-        director: "Tim Johnson",
-        releaseYear: 2015,
-        title: "Home",
-        posterUrl:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgBvtLbdotFhbnsspMrqg9spxq8Y_a9y1bIaXpgqBYLRTHtTvl"),
-    Movie(
-        director: "Guy Ritchie",
-        releaseYear: 2023,
-        title: "Operation Fortune",
-        posterUrl: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTnSuXyUtRNvTVgRqecf6adYdrkGHmFxe-_84RXazjtCvTLd1UI"),
-    Movie(
-        director: "David Leitch",
-        releaseYear: 2018,
-        title: "Deadpool 2",
-        posterUrl:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEaMyt21dd-I0jmKj9YO98a6F02iDp30uuitOjyVoIIp342NI0"),
-    Movie(
-        director: "Sam Hargrave",
-        releaseYear: 2020,
-        title: "Extraction",
-        posterUrl:
-            "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRELEr5bWH1Z9ZlYjofDbRoW0ToFJop6YlcJuVU8lAYt2peph_n"),
-    Movie(
-        director: "Rob Cohen",
-        releaseYear: 2018,
-        title: "hurricane heist",
-        posterUrl:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJDPMwFDj7aoEnNvlWSphJIKz2aOLsg_tX_96DBGV2iXTdDhOF"),
-  ];
+class MovieScreen extends StatefulWidget {
+  const MovieScreen({super.key});
+  @override
+  State<MovieScreen> createState() => MovieScreenState();
+}
 
+class MovieScreenState extends State<MovieScreen> {
+  final List<Movie> favoriteMovies = [];
+  final TextEditingController _movieTitle = TextEditingController();
+  final TextEditingController _movieDirector = TextEditingController();
+  final TextEditingController _moviePosterUrl = TextEditingController();
+  final TextEditingController _movieReleaseYear = TextEditingController();
   void showMovieDetails(BuildContext context, Movie movie) {
-     showDialog(
+    showDialog(
         context: context,
         builder: (context) {
           return AlertDialog(
@@ -79,19 +28,91 @@ class MovieScreen extends StatelessWidget {
                 SizedBox(
                   height: 10,
                 ),
-                Text("Director:${movie.director}",style: TextStyle(color: Colors.white)),
+                Text("Director:${movie.director}",
+                    style: TextStyle(color: Colors.white)),
                 SizedBox(
                   height: 10,
                 ),
-                Text("Release Year: ${movie.releaseYear}",style: TextStyle(color: Colors.white)),
+                Text("Release Year: ${movie.releaseYear}",
+                    style: TextStyle(color: Colors.white)),
               ],
             ),
             actions: [
               TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Close", style: TextStyle(color: Colors.white)),
+              )
+            ],
+          );
+        });
+  }
+
+  void saveMovieDetails() {
+    final myNewMovie = Movie(
+        director: _movieDirector.text,
+        releaseYear: int.parse(_movieReleaseYear.text),
+        title: _movieTitle.text,
+        posterUrl: _moviePosterUrl.text);
+    setState(() {
+      favoriteMovies.add(myNewMovie);
+    });
+    Navigator.pop(context);
+  }
+
+  void showAddMovieDialog(BuildContext context) {
+    //BuildContext  context
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Add a movie'),
+            content: Column(children: [
+              TextField(
+                controller: _movieTitle,
+                decoration: InputDecoration(
+                  hintText: "Enter the Movie Title",
+                  labelText: "Title",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 16,),
+              TextField(
+                controller: _movieDirector,
+                decoration: InputDecoration(
+                  hintText: "Enter the Movie Director",
+                  labelText: "Director",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              
+              SizedBox(height: 16,),
+              TextField(
+                controller: _moviePosterUrl,
+                decoration: InputDecoration(
+                  hintText: "Enter the Movie Poster URL",
+                  labelText: "Poster URL",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 16,),
+              TextField(
+                controller: _movieReleaseYear,
+                decoration: InputDecoration(
+                  hintText: "Enter the Movie Release Year",
+                  labelText: "Release Yrar",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ]),
+            actions: [
+              TextButton(onPressed: saveMovieDetails, child: Text("Save")),
+              TextButton(
                   onPressed: () {
                     Navigator.pop(context);
                   },
-                  child: Text("Close",style: TextStyle(color: Colors.white)),)
+                  child: Text("Cancel"))
             ],
           );
         });
@@ -103,12 +124,34 @@ class MovieScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("My Favorite Movies",style: TextStyle(color: Colors.white),),
+        title: Text(
+          "My Favorite Movies",
+          style: TextStyle(color: Colors.white),
+        ),
         shadowColor: Color(0xFF3232EB),
         backgroundColor: const Color.fromARGB(255, 65, 0, 243),
       ),
       body: SingleChildScrollView(
-        child: ListView.separated(
+          child: Column(children: [
+        ElevatedButton(
+          onPressed: () {
+            showAddMovieDialog(context);
+          },
+          style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              backgroundColor: Color.fromRGBO(112, 17, 238, 1),
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(
+                decorationStyle: TextDecorationStyle.double,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              )),
+          child: const Text('Add Movie'),
+        ),
+        ListView.separated(
           shrinkWrap: true,
           physics: NeverScrollableScrollPhysics(),
           itemCount: favoriteMovies.length,
@@ -118,7 +161,8 @@ class MovieScreen extends StatelessWidget {
           ),
           itemBuilder: (context, index) {
             final movie = favoriteMovies[index];
-            final imageHeight = screenWidth * 0.2; // Adjust this ratio as needed
+            final imageHeight =
+                screenWidth * 0.2; // Adjust this ratio as needed
 
             return Card(
               color: Colors.black,
@@ -126,7 +170,8 @@ class MovieScreen extends StatelessWidget {
                 leading: Image.network(
                   movie.posterUrl,
                   height: imageHeight,
-                  width: imageHeight * 0.7, // Adjust the image width proportionally
+                  width: imageHeight *
+                      0.7, // Adjust the image width proportionally
                   fit: BoxFit.contain,
                 ),
                 title: Text(movie.title, style: TextStyle(color: Colors.white)),
@@ -137,9 +182,7 @@ class MovieScreen extends StatelessWidget {
             );
           },
         ),
-      ),
+      ])),
     );
   }
 }
-
-
